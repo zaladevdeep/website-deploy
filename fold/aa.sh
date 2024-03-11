63d0d1879d8ff6e5c13fe58811fd007c06ac0083
Devdeepsinh Zala <76090365+zaladevdeep@users.noreply.github.com>
2024-03-11 16:58:02 +0530

    updates

--- fold/aa.sh
+++ fold/aa.sh
@@ -1,8 +1,3 @@
-new_cron_command='@reboot sudo systemctl start codedeploy-agent\nHello my name is devdeep' 
-
-# Create temporary file
-echo -e "$new_cron_command" > my_crontab
-
 # Overwrite crontab (use sudo only if necessary)
 sudo crontab my_crontab
 
