f8c795dec89f7eadb1c965cf2a9e67bc4e607c34
Devdeepsinh Zala <76090365+zaladevdeep@users.noreply.github.com>
2024-03-13 15:14:40 +0530

    chnages as on 13-03-2024

---
+++ fold/send_msg.py
@@ -0,0 +1,12 @@
+import requests
+def send_msg(message):
+    TOKEN = "6832260988:AAH9i3B-HGAN-M8B4TgCr6tfdXOcR2pJn_U"
+    chat_id = "5113043642"
+    # message = "hello from your telegram bot"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    # print(requests.get(url).json())
+    requests.get(url)
+i=0
+while i<10:
+    send_msg(f"hello {i}")
+    i+=1
